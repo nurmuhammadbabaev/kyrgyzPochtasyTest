eaa38c40b1fb23cf729e9bc21d05666d4ccf4438
Nurmuhammed <nurmuhammadbabaev@gmail.com>
2022-05-28 23:01:46 +0600

    Finished project Kyrgyz Pochtasy12

--- src/main/java/kyrgyzPochtasy/Main.java
+++
@@ -1,4 +0,0 @@
-package kyrgyzPochtasy;
-
-public class Main {
-}
--- src/main/java/kyrgyzPochtasy/controller/EmployeeController.java
+++ src/main/java/kyrgyzPochtasy/controller/EmployeeController.java
@@ -18,42 +18,58 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+
     @GetMapping
-    public String getAllEmployee(Model model) {
-        model.addAttribute("employees", employeeService.getAllEmployee());
+    public String getMainPage(Model model) {
+        model.addAttribute("employees", "Main Page");
+        //Главная страница
         return "main-page";
+    }
+
+    @GetMapping("/all")
+    public String getAllEmployee(Model model) {
+        //Получения всех сотрудников
+        model.addAttribute("employees", employeeService.getAllEmployee());
+        //Страница всех сотрудников
+        return "allEmployees";
     }
 
 
     @GetMapping("/new")
     public String newEmployee(@ModelAttribute("employee") Employee employee) {
-        //Добавляем компанию в БД
-
+        //Страница для добавления сотрудников в БД
         return "addEmployee";
     }
 
     @PostMapping("/save")
     public String addEmployee(@ModelAttribute("employee") Employee employee) {
+        //Добавляем сотрудников в БД
         employeeService.saveEmployee(employee);
+        //После добавления вернемся обратно в главную страницу
         return "redirect:/employees";
     }
 
     @GetMapping("/{id}/edit")
     public String editEmployee(@PathVariable("id") int id, Model model) {
         model.addAttribute("employee", employeeService.getEmployeeById(id));
+        //Страница для изменения сотрудников
         return "updateEmployee";
     }
 
     @PatchMapping("/{id}")
     public String updateEmployee(@ModelAttribute("employee") Employee employee, @PathVariable("id") int id, Model model) {
+        //Изменяем сотрудников
         employeeService.update(id, employee);
         model.addAttribute("employee", employee);
-        return "redirect:/employees";
+        //После изменения вернемся обратно на страницу all
+        return "redirect:/employees/all";
     }
 
     @GetMapping("/{id}")
     public String deleteEmployee(@PathVariable("id") int id) {
+        //Удаляем сотрудников
         employeeService.removeEmployeeById(id);
-        return "redirect:/employees";
+        //После удаления сотрудников вернемся обратно на страницу all
+        return "redirect:/employees/all";
     }
 }
